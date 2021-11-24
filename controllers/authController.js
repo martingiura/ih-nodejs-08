c970fd9fde2169f1e3b559f57609e68f5da45c06
Martin Giura <martin.giura@gmail.com>
2021-11-24 13:31:48 -0600

    error messages personalizados para correo tambien

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+// ./controllers/authController.js
+
 const User		= require("./../models/User")
 const bcryptjs = require("bcryptjs")
 
@@ -27,20 +29,52 @@ exports.register = async (req, res) => {
 		return
 	}
 
+	// => B) VALIDACIÓN - FORTALECIMIENTO DE PASSWORD
+	// VERIFIQUE QUE EL PASSWORD TENGA 6 CARACTERES, 
+	// MÍNIMO UN NÚMERO Y UNA MAYÚSCULA.
+	// REGEX - CONJUNTO DE REGLAS QUE AUDITAN UN TEXTO PLANO
+	const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
+
+	if(!regex.test(password)){
+		
+		res.render("auth/signup", {
+			errorMessage: "Tu password debe de contener 6 caracteres, mínimo un número y una mayúscula."
+		})		
+
+		return
+	}
+
+
+
+
 
 	// 2. ENCRIPTACIÓN DE PASSWORD 🚩🚩🚩
-	const salt = await bcryptjs.genSalt(10)
-	const passwordEncriptado = await bcryptjs.hash(password, salt)
-	
-	const newUser = await User.create({
-		username,
-		email,
-		passwordEncriptado
-	}) 
 
-	console.log(newUser)
+	try {
+		const salt = await bcryptjs.genSalt(10)
+		const passwordEncriptado = await bcryptjs.hash(password, salt)
+		
+		const newUser = await User.create({
+			username,
+			email,
+			passwordEncriptado
+		}) 
+
+		console.log(newUser)
+		
+		// 3. REDIRECCIÓN DE USUARIO
+		res.redirect("/")
+
+	} catch (error) {
+
+		console.log(error)
+
+		res.status(500).render("auth/signup", {
+			errorMessage: "Hubo un error con la validez de tu correo. Intenta nuevamente. No dejes espacios y usa minúsculas."
+		})
+
+	}
+
 	
-	// 3. REDIRECCIÓN DE USUARIO
-	res.redirect("/")
 
 }
--- models/User.js
+++ models/User.js
@@ -7,7 +7,14 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
 	username: String,
-	email: String,
+	email: {
+		type: String,
+		required: [true, "Email es requerido."], // QUE NO ESTÉ VACÍO
+		match: [/^\S+@\S+\.\S+$/, "Por favor, ingresa un email válido."], // REGEX DEL EMAIL
+		unique: true, // EMAIL ÚNICO EN LA BASE DE DATOS
+		lowercase: true, // MINÚSCULAS
+		trim: true // SIN ESPACIOS VACÍOS
+	},
 	passwordEncriptado: String
 })
 
